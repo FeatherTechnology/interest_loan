8fafcb1517fb2f082e27228073010610b82ece90
Vasantharaj <vasanth3523av@gmail.com>
2025-06-17 17:17:56 +0530

    corrections in opening balance
    corrections in opening balance

--- api/accounts_files/accounts/opening_balance.php
+++ api/accounts_files/accounts/opening_balance.php
@@ -8,7 +8,7 @@ $current_date = date('Y-m-d');
 
 // <------------------------------------------------------------------ Collection Credit Hand Cash --------------------------------------------------------->
 
-$c_cr_hc_qry = $pdo->query("SELECT SUM(collection_amnt) AS coll_cr_amnt FROM accounts_collect_entry WHERE collection_mode = 1 AND DATE(created_on) <= '$current_date' - INTERVAL 1 DAY ");
+$c_cr_hc_qry = $pdo->query("SELECT SUM(collection_amnt) AS coll_cr_amnt FROM accounts_collect_entry WHERE collection_mode = 1 AND DATE(created_on) < '$current_date' ");
 if ($c_cr_hc_qry->rowCount() > 0) {
     $c_cr_hc = $c_cr_hc_qry->fetch()['coll_cr_amnt'];
 } else {
@@ -17,7 +17,7 @@ if ($c_cr_hc_qry->rowCount() > 0) {
 
 // <------------------------------------------------------------------ Collection Credit Bank Cash --------------------------------------------------------->
 
-$c_cr_bc_qry = $pdo->query("SELECT SUM(collection_amnt) AS coll_cr_amnt FROM accounts_collect_entry WHERE collection_mode = 2 AND DATE(created_on) <= '$current_date' - INTERVAL 1 DAY ");
+$c_cr_bc_qry = $pdo->query("SELECT SUM(collection_amnt) AS coll_cr_amnt FROM accounts_collect_entry WHERE collection_mode = 2 AND DATE(created_on) < '$current_date' ");
 if ($c_cr_bc_qry->rowCount() > 0) {
     $c_cr_bc = $c_cr_bc_qry->fetch()['coll_cr_amnt'];
 } else {
@@ -26,7 +26,7 @@ if ($c_cr_bc_qry->rowCount() > 0) {
 
 // <------------------------------------------------------------------ Loan Issue Debit Hand Cash --------------------------------------------------------->
 
-$li_db_hc_qry = $pdo->query("SELECT COALESCE(SUM(cash),0) AS li_db_hc_amt FROM loan_issue WHERE DATE(created_on)<= '$current_date' - INTERVAL 1 DAY ");
+$li_db_hc_qry = $pdo->query("SELECT COALESCE(SUM(cash),0) AS li_db_hc_amt FROM loan_issue WHERE DATE(created_on)< '$current_date' ");
 if ($li_db_hc_qry->rowCount() > 0) {
     $li_db_hc = $li_db_hc_qry->fetch()['li_db_hc_amt'];
 } else {
@@ -35,7 +35,7 @@ if ($li_db_hc_qry->rowCount() > 0) {
 
 // <------------------------------------------------------------------ Loan Issue Debit Bank Cash --------------------------------------------------------->
 
-$li_db_bc_qry = $pdo->query("SELECT COALESCE(SUM(cheque_val) + SUM(transaction_val),0) AS li_db_bc_amnt FROM loan_issue WHERE DATE(created_on) <= '$current_date' - INTERVAL 1 DAY");
+$li_db_bc_qry = $pdo->query("SELECT COALESCE(SUM(cheque_val) + SUM(transaction_val),0) AS li_db_bc_amnt FROM loan_issue WHERE DATE(created_on) < '$current_date'");
 if ($li_db_bc_qry->rowCount() > 0) {
     $li_db_bc = $li_db_bc_qry->fetch()['li_db_bc_amnt'];
 } else {
@@ -44,7 +44,7 @@ if ($li_db_bc_qry->rowCount() > 0) {
 
 // <---------------------------------------------------------------------- Expense Debit Hand Cash --------------------------------------------------------->
 
-$ex_db_hc_qry = $pdo->query("SELECT SUM(amount) AS exp_db_hc_amnt FROM expenses WHERE collection_mode = 1 AND DATE(created_on) <= '$current_date' - INTERVAL 1 DAY"); 
+$ex_db_hc_qry = $pdo->query("SELECT SUM(amount) AS exp_db_hc_amnt FROM expenses WHERE collection_mode = 1 AND DATE(created_on) < '$current_date'"); 
 if ($ex_db_hc_qry->rowCount() > 0) {
     $ex_db_hc = $ex_db_hc_qry->fetch()['exp_db_hc_amnt'];
 } else {
@@ -53,7 +53,7 @@ if ($ex_db_hc_qry->rowCount() > 0) {
 
 // <---------------------------------------------------------------------- Expense Debit Bank Cash --------------------------------------------------------->
 
-$ex_db_bc_qry = $pdo->query("SELECT SUM(amount) AS exp_db_bc_amnt FROM expenses WHERE collection_mode = 2 AND DATE(created_on) <= '$current_date' - INTERVAL 1 DAY "); 
+$ex_db_bc_qry = $pdo->query("SELECT SUM(amount) AS exp_db_bc_amnt FROM expenses WHERE collection_mode = 2 AND DATE(created_on) < '$current_date' "); 
 if ($ex_db_bc_qry->rowCount() > 0) {
     $ex_db_bc = $ex_db_bc_qry->fetch()['exp_db_bc_amnt'];
 } else {
@@ -62,7 +62,7 @@ if ($ex_db_bc_qry->rowCount() > 0) {
 
 // <---------------------------------------------------------------- Other Transaction Credit Hand Cash ----------------------------------------------------->
 
-$ot_cr_hc_qry = $pdo->query("SELECT SUM(amount) AS ot_cr_hc_amnt FROM other_transaction WHERE collection_mode = 1 AND type = 1 AND DATE(created_on) <= '$current_date' - INTERVAL 1 DAY "); 
+$ot_cr_hc_qry = $pdo->query("SELECT SUM(amount) AS ot_cr_hc_amnt FROM other_transaction WHERE collection_mode = 1 AND type = 1 AND DATE(created_on) < '$current_date' "); 
 if ($ot_cr_hc_qry->rowCount() > 0) {
     $ot_cr_hc = $ot_cr_hc_qry->fetch()['ot_cr_hc_amnt'];
 } else {
@@ -71,7 +71,7 @@ if ($ot_cr_hc_qry->rowCount() > 0) {
 
 // <---------------------------------------------------------------- Other Transaction Debit Hand Cash ----------------------------------------------------->
 
-$ot_db_hc_qry = $pdo->query("SELECT SUM(amount) AS ot_db_hc_amnt FROM other_transaction WHERE collection_mode = 1 AND type = 2 AND DATE(created_on) <= '$current_date' - INTERVAL 1 DAY"); //Hand Cash //debit
+$ot_db_hc_qry = $pdo->query("SELECT SUM(amount) AS ot_db_hc_amnt FROM other_transaction WHERE collection_mode = 1 AND type = 2 AND DATE(created_on) < '$current_date'"); //Hand Cash //debit
 if ($ot_db_hc_qry->rowCount() > 0) {
     $ot_db_hc = $ot_db_hc_qry->fetch()['ot_db_hc_amnt'];
 } else {
@@ -80,7 +80,7 @@ if ($ot_db_hc_qry->rowCount() > 0) {
 
 // <---------------------------------------------------------------- Other Transaction Credit Bank Cash ----------------------------------------------------->
 
-$ot_cr_bc_qry = $pdo->query("SELECT SUM(amount) AS ot_amnt FROM other_transaction WHERE collection_mode = 2 AND type = 1 AND DATE(created_on) <= '$current_date' - INTERVAL 1 DAY ");
+$ot_cr_bc_qry = $pdo->query("SELECT SUM(amount) AS ot_amnt FROM other_transaction WHERE collection_mode = 2 AND type = 1 AND DATE(created_on) < '$current_date' ");
 if ($ot_cr_bc_qry->rowCount() > 0) {
     $ot_cr_bc = $ot_cr_bc_qry->fetch()['ot_amnt'];
 } else {
@@ -89,7 +89,7 @@ if ($ot_cr_bc_qry->rowCount() > 0) {
 
 // <---------------------------------------------------------------- Other Transaction Debit Bank Cash ----------------------------------------------------->
 
-$ot_db_bc_qry = $pdo->query("SELECT SUM(amount) AS ot_amnt FROM other_transaction WHERE collection_mode = 2 AND type = 2 AND DATE(created_on) <= '$current_date' - INTERVAL 1 DAY"); 
+$ot_db_bc_qry = $pdo->query("SELECT SUM(amount) AS ot_amnt FROM other_transaction WHERE collection_mode = 2 AND type = 2 AND DATE(created_on) < '$current_date'"); 
 if ($ot_db_bc_qry->rowCount() > 0) {
     $ot_db_bc = $ot_db_bc_qry->fetch()['ot_amnt'];
 } else {
