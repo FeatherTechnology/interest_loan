4951bb5831283977e5e3da5efb490eca9fdfd4b0
Vasantharaj <vasanth3523av@gmail.com>
2025-07-01 10:10:26 +0530

    Correction in Existing and Repromotion
    Correction in Existing and Repromotion

--- api/customer_data_files/get_repromotion_list.php
+++ api/customer_data_files/get_repromotion_list.php
@@ -18,16 +18,41 @@ if (isset($_POST['repromotion_details']) && !empty($_POST['repromotion_details']
 }
 
 $customerQry = $pdo->query("SELECT 
-                le.id as loan_entry_id , cc.id, cc.cus_id, cc.aadhar_number, CONCAT(cc.first_name, ' ', COALESCE(cc.last_name, '')) AS cus_name, anc.areaname AS area, lnc.linename, bc.branch_name, cc.mobile1, cs.status as c_sts, cs.sub_status as c_substs, rc.repromotion_detail, rc.created_on as created, cc.created_on as cus_created FROM customer_creation cc 
-                LEFT JOIN line_name_creation lnc ON cc.line = lnc.id 
-                LEFT JOIN area_name_creation anc ON cc.area = anc.id 
-                LEFT JOIN area_creation ac ON cc.line = ac.line_id 
-                LEFT JOIN branch_creation bc ON ac.branch_id = bc.id 
-                LEFT JOIN loan_entry le ON le.cus_id = cc.cus_id 
-                LEFT JOIN customer_status cs ON le.id = cs.loan_entry_id  
-                LEFT JOIN repromotion_customer rc ON le.id = rc.loan_entry_id
-                WHERE (cs.status = 5 || cs.status = 6 || cs.status = 8 || cs.status = 9) $whereCondition
-                ORDER BY cc.id DESC");
+    le.id as loan_entry_id,
+    cc.id,
+    cc.cus_id,
+    cc.aadhar_number,
+    CONCAT(cc.first_name, ' ', COALESCE(cc.last_name, '')) AS cus_name,
+    anc.areaname AS area,
+    lnc.linename,
+    bc.branch_name,
+    cc.mobile1,
+    cs.status as c_sts,
+    cs.sub_status as c_substs,
+    rc.repromotion_detail,
+    rc.created_on as created,
+    cc.created_on as cus_created
+
+FROM customer_creation cc
+
+LEFT JOIN (
+    SELECT MAX(id) AS latest_loan_id, cus_id
+    FROM loan_entry
+    GROUP BY cus_id
+) latest_loan ON cc.cus_id = latest_loan.cus_id
+
+LEFT JOIN loan_entry le ON le.id = latest_loan.latest_loan_id
+LEFT JOIN customer_status cs ON le.id = cs.loan_entry_id
+LEFT JOIN repromotion_customer rc ON le.id = rc.loan_entry_id
+LEFT JOIN line_name_creation lnc ON cc.line = lnc.id
+LEFT JOIN area_name_creation anc ON cc.area = anc.id
+LEFT JOIN area_creation ac ON cc.line = ac.line_id
+LEFT JOIN branch_creation bc ON ac.branch_id = bc.id
+
+WHERE (cs.status = 5 OR cs.status = 6 OR cs.status = 8 OR cs.status = 9) 
+$whereCondition
+
+ORDER BY cc.id DESC");
 
 if ($customerQry->rowCount() > 0) {
     while ($customerRow = $customerQry->fetch(PDO::FETCH_ASSOC)) {
